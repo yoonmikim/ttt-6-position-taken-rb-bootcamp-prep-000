441959efcc8eb58a30ec97966d14f282772d7677
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-10-25 17:14:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index) 
-  if board == " " || board == "" || board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board == "X" || board == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
